251ce3a5ef878e31aaad9da643d5726c7c306753
gersonbreno <88159573+gersonbreno@users.noreply.github.com>
2023-05-26 16:37:03 -0300

    Add files via upload

---
+++ site de top de animes/js/teste.js
@@ -0,0 +1,10 @@
+document.getElementById('açao').addEventListener('click', function (notificar) {
+  notificar.preventDefault()
+
+  alert('Essa opção esté em manutenção aguarde')
+})
+document.getElementById('romance').addEventListener('click', function (notrm) {
+  notrm.preventDefault()
+
+  alert('Essa opção esté em manutenção aguarde')
+})
